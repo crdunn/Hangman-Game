1443e279f7786bd8dcdb31d29745813a02f7be10
Charles Dunn <charlesdunn@Charless-MBP.home>
2018-01-02 19:36:53 -0500

    Finally pushed this

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 //var unknownArray 
 //var userGuess
 
-var wordsArray = ["hypertext", "cascading", "java", "query", "firebase"]
+var wordsArray = ["hypertext", "cascading", "java", "query", "firebase", "database", "react"];
 
 var singleWord = [];
 
@@ -18,37 +18,99 @@ var guessesLeft = 10;
 
 function newWord () {
 
+	guessesLeft = 10;
+	guessLetters = [];
+
 	var randNum = Math.floor(Math.random()*wordsArray.length);
 
 	singleWord = wordsArray[randNum].split("");
 
+	for (var j = 0; j < singleWord.length; j++) {
+
+		wordProgress[j] = " _ " 
+	};
+
 	console.log(singleWord);
 
-	for (var i = 0; i < singleWord.length; i++){
+	render();
 
-		document.getElementById("gameWord").append(" _ ");
-	};
 };
 
+function render (){
 
+	var finishedWord = 0;
 
+	document.getElementById("gameWord").innerHTML = "";
+	document.getElementById("guessedLetters").innerHTML = "";
+	document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
-function userguess() {
+	for (var i = 0; i < wordProgress.length; i++){
 
-	for (var i = 0; i < singleWord.length; i++){
+		document.getElementById("gameWord").append(wordProgress[i]);
 
-		if (key = singleWord[i]){
-
-			document.getElementById("gameWord").append(" " + key + " ");
-
-		} else {
-
-			document.getElementById("gameWord").append(" _ ");
+		if (wordProgress[i] != " _ "){
+			finishedWord++
+		};
+		console.log(finishedWord);
 
 		};
 
+	for (var j = 0; j < guessLetters.length; j++){
+
+		document.getElementById("guessedLetters").append(" "+guessLetters[j]+" ");
 	};
 
+	if (finishedWord === wordProgress.length){
+		alert("Victory!");
+		finishedWord = 0;
+	};
+
+
 };
+
+document.onkeyup = function userguess (event){
+
+	if (guessesLeft === 0){
+		alert("You lose!");
+		newWord();
+	} else {
+		guessesLeft--
+		var letterInword = false
+
+		for (var i = 0; i < singleWord.length; i++) {
+
+			if (event.key === singleWord[i]) {
+
+				wordProgress[i] = " "+ event.key + " ";
+				letterInword = true;
+			};
+		};
+
+		guessLetters.push(event.key);
+
+		if (letterInword){guessesLeft++};
+
+		render();
+	};
+};
+
+
+// function userguess() {
+
+// 	for (var i = 0; i < singleWord.length; i++){
+
+// 		if (key = singleWord[i]){
+
+// 			document.getElementById("gameWord").append(" " + key + " ");
+
+// 		} else {
+
+// 			document.getElementById("gameWord").append(" _ ");
+
+// 		};
+
+// 	};
+
+// };
 
 newWord();
